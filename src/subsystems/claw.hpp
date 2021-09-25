45266a4c40d2e78f0f5469e48a86439da72d1ec5
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2021-09-24 19:24:29 -0500

    [3/4?] subsystems/claw make more usable
    make work

--- src/subsystems/claw.hpp
+++ src/subsystems/claw.hpp
@@ -1,13 +1,21 @@
 #include "../components/piston.hpp"
 #include "../design_consts.hpp"
 
-class
+class Claw
 {
 private:
-    Piston piston = Piston(CONFIG_CLAW_PORT, CONFIG_CLAW_REVERSED);
+    Piston piston = Piston(HARDWARE::CLAW_PORT, HARDWARE::CLAW_REVERSED);
     bool state = false;
 
+    pros::Motor mtr1 = HARDWARE::CLAW_ARM_MOTOR1;
+    pros::Motor mtr2 = HARDWARE::CLAW_ARM_MOTOR2;
+
 public:
+    Claw()
+    {
+        mtr1.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+        mtr2.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    };
     void Clasp()
     {
         state = true;
@@ -24,9 +32,10 @@ public:
         return state ? this->piston.retract() : this->piston.extend();
     }
 
-    void ArmUP() {};
-    void ArmUp(int velocity) {};
-
-    void ArmDown() {};
-
-} claw;
+    void ArmMove(float v)
+    {
+        v = v / MAX_MOTOR_VOLTAGE * 128;
+        mtr1.move(v);
+        mtr2.move(v);
+    };
+};
