18863f3b40ed46af49c842636e23a86c619f956d
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2021-10-21 11:12:33 -0500

    main auton selection screen

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,34 @@ void claw_monitor()
 		}
 }
 
+enum AUTON_ROUTINES
+{
+	awp_right,
+	awp_left,
+	neumogo_front,
+	auton_routine_none
+};
+static AUTON_ROUTINES SELECTED_AUTON_ROUTINE = awp_right;
+
+void on_screen_button()
+{
+	switch (SELECTED_AUTON_ROUTINE)
+	{
+	case awp_right:
+		SELECTED_AUTON_ROUTINE = awp_left;
+		break;
+	case awp_left:
+		SELECTED_AUTON_ROUTINE = neumogo_front;
+		break;
+	case neumogo_front:
+		SELECTED_AUTON_ROUTINE = auton_routine_none;
+		break;
+	case auton_routine_none:
+		SELECTED_AUTON_ROUTINE = awp_right;
+		break;
+	}
+}
+
 /**
  * Runs initialization code. This occurs as soon as the program is started.
  *
@@ -29,7 +57,6 @@ void initialize()
 {
 	printf("init\n");
 	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
 
 	claw.init();
 	auto t = pros::Task(claw_monitor);
@@ -37,6 +64,16 @@ void initialize()
 	drive.init();
 
 	printf("inited\n");
+}
+
+void printAutonRoutines()
+{
+	auto r = SELECTED_AUTON_ROUTINE;
+	pros::lcd::print(1, "AWP Right %s", r == awp_right ? "yes" : "no");
+	pros::lcd::print(2, "AWP Left %s", r == awp_left ? "yes" : "no");
+	pros::lcd::print(3, "Front Neutral MOGO %s", r == neumogo_front ? "yes" : "no");
+	pros::lcd::print(4, "None %s", r == auton_routine_none ? "yes" : "no");
+	pros::lcd::print(5, "make sure arm down btw", r == auton_routine_none ? "yes" : "no");
 }
 
 /**
@@ -58,9 +95,16 @@ void disabled() {}
 void competition_initialize()
 {
 	printf("COMPINITSTART\n");
-	//claw.ArmSetNum(0);
-	//pros::delay(90);
-	//claw.WaitUntilSettled();
+	pros::lcd::register_btn0_cb(on_screen_button);
+
+	while (true)
+	{
+		pros::lcd::clear();
+		printAutonRoutines();
+	}
+
+	pros::lcd::clear();
+
 	printf("COMPINIT\n");
 }
 
@@ -140,7 +184,6 @@ void auton_awp2()
 		drive.chassis->getModel()->forward(-newOutput);
 		pros::delay(10);
 	}
-	//drive.chassis->moveDistance(1_in);
 	printf("REACHED\n");
 
 	claw.Clasp();
@@ -157,28 +200,11 @@ void auton_yellow()
 	drive.chassis->moveDistanceAsync(d - 5_in);
 	drive.chassis->turnAngleAsync(-5_deg);
 	printf("2\n");
-	//claw.WaitUntilSettled();
-	//printf("3\n");
 	drive.chassis->waitUntilSettled();
 	drive.chassis->setMaxVelocity(60);
 	printf("4\n");
 	drive.chassis->moveDistance(6_in);
 
-	//auto exampleController = okapi::IterativeControllerFactory::posPID(0.005, 0.001, 0);
-	//exampleController.setTarget(75);
-	//while (!exampleController.isSettled())
-	//{
-	//	double in = ultrasonic.controllerGet();
-	//	double newOutput = exampleController.step(in);
-
-	//	static int count = 0;
-	//	count++;
-	//	if (!(count % 30))
-	//		printf("%f:%f\n", in, newOutput);
-	//	drive.chassis->getModel()->forward(-newOutput);
-	//	pros::delay(10);
-	//}
-	//drive.chassis->moveDistance(1_in);
 	printf("REACHED\n");
 
 	claw.Clasp();
@@ -195,10 +221,26 @@ void autonomous()
 
 	claw.Motor2Hold(false);
 	pros::Task us = pros::Task(print);
-	claw.ArmSet(0);
-	//auton_awp();
-	//auton_awp2();
-	auton_yellow();
+	if (SELECTED_AUTON_ROUTINE == awp_right)
+	{
+		claw.ArmSetNum(1);
+		claw.ArmSetNum(0);
+		auton_awp();
+	}
+	else if (SELECTED_AUTON_ROUTINE == awp_left)
+	{
+		claw.ArmSetNum(1);
+		claw.ArmSetNum(0);
+		auton_awp2();
+	}
+	else if (SELECTED_AUTON_ROUTINE == neumogo_front)
+	{
+		auton_yellow();
+	}
+	else
+	{
+	}
+
 	claw.Motor2Hold(true);
 }
 
