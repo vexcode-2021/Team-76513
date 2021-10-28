1a4e9d49ce996760a2e138dd842b4bce3981bcfd
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2021-10-27 21:35:25 -0500

    components/piston change default claw position

--- src/components/piston.cpp
+++ src/components/piston.cpp
@@ -10,7 +10,7 @@ Piston::Piston(const char port, bool reversed)
 }
 void Piston::init(const char port, bool reversed)
 {
-    this->port = pros::ADIDigitalOut(port, false);
+    this->port = pros::ADIDigitalOut(port, true);
     this->reversed = reversed;
 }
 
