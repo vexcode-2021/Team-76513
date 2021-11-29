d026324a4ac30b784a9ebc2d51c3509616606e45
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2021-11-28 20:08:35 -0600

    subsystems/drivetrain: curve controller input
    - this isn't the final iteration, just a simple thing to make small precise
      movements easier for the driver
    
    vex-76513/b-robot-2021#16

--- src/subsystems/drivetrain.cpp
+++ src/subsystems/drivetrain.cpp
@@ -19,16 +19,21 @@ void Drivetrain::init()
             .buildOdometry();
 }
 
+double curve(double n)
+{
+    return n * n;
+}
+
 void Drivetrain::drive(Controller m_c)
 {
     if (mode == DRIVE_MODE_TANK)
         chassis->getModel()->tank(
-            m_c.getAnalog(ButtonMapping::tank_drive[0]),
-            m_c.getAnalog(ButtonMapping::tank_drive[1]));
+            curve(m_c.getAnalog(ButtonMapping::tank_drive[0])),
+            curve(m_c.getAnalog(ButtonMapping::tank_drive[1])));
     else if (mode == DRIVE_MODE_ARCADE)
         chassis->getModel()->arcade(
-            m_c.getAnalog(ButtonMapping::arcade_drive[0]),
-            m_c.getAnalog(ButtonMapping::arcade_drive[1]));
+            curve(m_c.getAnalog(ButtonMapping::arcade_drive[0])),
+            curve(m_c.getAnalog(ButtonMapping::arcade_drive[1])));
     else
         toggleMode();
 }
