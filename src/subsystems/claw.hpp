390e24bed09902d023825cedaedce4b08ab2c398
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2022-02-17 15:07:32 -0600

    subsystems/claw: single motor cleanup + get pos

--- src/subsystems/claw.hpp
+++ src/subsystems/claw.hpp
@@ -7,24 +7,20 @@ class Claw
 private:
     Piston piston = Piston(HARDWARE::CLAW_PORT, HARDWARE::CLAW_REVERSED, false);
 
-    okapi::MotorGroup mtr = okapi::MotorGroup(HARDWARE::CLAW_ARM_MOTORS);
+    okapi::Motor mtr = HARDWARE::CLAW_ARM_MOTOR1;
 
     int curr = 0;
 
-    std::shared_ptr<okapi::AsyncPositionController<double, double>> controllerl;
-    std::shared_ptr<okapi::AsyncPositionController<double, double>> controllerr;
+    std::shared_ptr<okapi::AsyncPositionController<double, double>> controller;
 
 public:
     Claw() {}
     void init()
     {
-        mtr.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
+        mtr.setBrakeMode(okapi::AbstractMotor::brakeMode::brake);
         mtr.setEncoderUnits(okapi::AbstractMotor::encoderUnits::degrees);
-        // const okapi::IterativePosPIDController::Gains gains = {0.0015, 0.0007, 0.00004};
-        // const okapi::IterativePosPIDController::Gains gains = {0.0072,0.008,0.00007 };
         const okapi::IterativePosPIDController::Gains gains = {0.00002, 0.008, 0.00014};
-        controllerl = okapi::AsyncPosControllerBuilder().withMotor(HARDWARE::CLAW_ARM_MOTOR2).withGains(gains).withSensor(HARDWARE::POTL).build();
-        controllerr = okapi::AsyncPosControllerBuilder().withMotor(HARDWARE::CLAW_ARM_MOTOR1).withGains(gains).withSensor(HARDWARE::POTR).build();
+        controller = okapi::AsyncPosControllerBuilder().withMotor(mtr).withGains(gains).withSensor(HARDWARE::POTL).build();
 
         ArmSetNum(0);
     };
@@ -49,23 +45,23 @@ public:
     {
         double val = v;
         double valL = (val * ((HARDWARE::LMAX - HARDWARE::LMIN) / 90)) + HARDWARE::LMIN;
-        double valR = (val * ((HARDWARE::RMAX - HARDWARE::RMIN) / 90)) + HARDWARE::RMIN;
 
-        printf("%f %f %f\n", val, valL, valR);
+        //printf("%f %f\n", val, valL);
 
-        controllerl->setTarget(valL);
-        controllerr->setTarget(valR);
+        controller->setTarget(valL);
     }
 
     void ArmSetRelative(double n)
     {
         const double minvalL = (CLAW_CONF::armPos[0] * ((HARDWARE::LMAX - HARDWARE::LMIN) / 90)) + HARDWARE::LMIN;
-        const double minvalR = (CLAW_CONF::armPos[0] * ((HARDWARE::RMAX - HARDWARE::RMIN) / 90)) + HARDWARE::RMIN;
 
         double valL = (n * ((HARDWARE::LMAX - HARDWARE::LMIN) / 90));
-        double valR = (n * ((HARDWARE::RMAX - HARDWARE::RMIN) / 90));
-        controllerl->setTarget(std::max(controllerl->getTarget() + valL, minvalL));
-        controllerr->setTarget(std::max(controllerr->getTarget() + valR, minvalR));
+        controller->setTarget(std::max(controller->getProcessValue() + valL, minvalL));
+    }
+    double Get()
+    {
+        return controller->getProcessValue() * (90.0 / (HARDWARE::LMAX - HARDWARE::LMIN)) - 22;
+        // idk y -22 but it helps
     }
 
     void ArmUp()
@@ -81,11 +77,11 @@ public:
 
     void WaitUntilSettled()
     {
-        if (HARDWARE::PROTO) return;
+        if (HARDWARE::PROTO)
+            return;
 
-pros::delay(25);
-        controllerr->waitUntilSettled();
-        controllerl->waitUntilSettled();
+        pros::delay(25);
+        controller->waitUntilSettled();
     }
 
     void ArmSetNum(const int n)
@@ -94,7 +90,7 @@ pros::delay(25);
         curr = std::clamp(n, 0, CLAW_CONF::ARM_POS_LEN - 1);
         ArmSet(CLAW_CONF::armPos[curr]);
     }
- void ArmSetNumWait(const int n)
+    void ArmSetNumWait(const int n)
     {
         ArmSetNum(n);
         WaitUntilSettled();
