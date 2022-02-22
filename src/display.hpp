ea60fcfad19837ed05188a7893a73665c03553f9
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2022-02-22 11:40:01 -0600

    PID_TUNING

--- src/display.hpp
+++ src/display.hpp
@@ -6,8 +6,6 @@ private:
     pros::Controller master();
     pros::Controller partner = pros::Controller(pros::E_CONTROLLER_PARTNER);
 
-    std::vector<double> vals = {};
-
     bool curr_brake = false;
 
     void brakeStuff()
@@ -15,9 +13,37 @@ private:
         if (pros::c::controller_get_digital_new_press(pros::E_CONTROLLER_PARTNER, ButtonMapping::drive_brake_toggle))
         {
             curr_brake ^= 1;
-            //xor equals 1 means toggle a boolean
+            // xor equals 1 means toggle a boolean
         }
         drive.brake(curr_brake);
+    }
+
+    const bool PID_TUNING = true;
+    int index = 0;
+    std::valarray<double> myvals = {1, 1, 1};
+    std::valarray<double> factors = {1e-4, 2, 1};
+
+    void pidTune()
+    {
+        if (pros::c::controller_get_digital_new_press(pros::E_CONTROLLER_PARTNER, pros::E_CONTROLLER_DIGITAL_A))
+        {
+            index = (index + 1) % 3;
+        }
+        if (pros::c::controller_get_digital_new_press(pros::E_CONTROLLER_PARTNER, pros::E_CONTROLLER_DIGITAL_UP))
+        {
+            myvals[index] *= 1.1;
+            mysetpid();
+        }
+        else if (pros::c::controller_get_digital_new_press(pros::E_CONTROLLER_PARTNER, pros::E_CONTROLLER_DIGITAL_DOWN))
+        {
+            myvals[index] /= 1.1;
+            mysetpid();
+        }
+    }
+    void mysetpid()
+    {
+        auto ans = (myvals * factors);
+        std::static_pointer_cast<okapi::ChassisControllerPID>(std::static_pointer_cast<okapi::DefaultOdomChassisController>(drive.chassis)->getChassisController())->setGains({ans[0], ans[1], ans[2]}, {}, {});
     }
 
     void statusPrint()
@@ -25,12 +51,20 @@ private:
         std::string printing = curr_brake ? "br" : "co";
 
         partner.print(1, 0, "%2s", printing.c_str());
+
+        if (PID_TUNING)
+        {
+            partner.print(2, 0, "%4.0f %4.0f, %4.0f ", myvals[0], myvals[1], myvals[2]);
+        }
     }
 
 public:
     void oneLoop()
     {
         brakeStuff();
+
+        if (PID_TUNING)
+            pidTune();
 
         statusPrint();
     };
